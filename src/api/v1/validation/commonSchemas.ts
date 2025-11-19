c886e19cb33132626e465c7d3e7053bcc5cf135e
Paramdeep Singh <psingh20@rrc.ca>
2025-11-18 21:40:29 -0600

    introduing the  reusable errorResponse schema and wire into endpoints

---
+++ src/api/v1/validation/commonSchemas.ts
@@ -0,0 +1,15 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     ErrorResponse:
+ *       type: object
+ *       properties:
+ *         statusCode:
+ *           type: integer
+ *           example: 404
+ *         message:
+ *           type: string
+ *           example: "Employee not found"
+ */
+export {};
